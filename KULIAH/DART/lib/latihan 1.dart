fd494b4b17c55782ed845c2c5820ad0726acff38
Adeavicka Madani <adeavickamadani19@gmail.com>
2025-08-26 11:48:39 +0900

    aplikasi biodata

---
+++ KULIAH/DART/lib/latihan 1.dart
@@ -0,0 +1,13 @@
+void main() {
+  String nama = "Adea";
+  double npm = 07352311102;
+  num umur = 19;
+  dynamic tinggi = 163;
+  int kelahiran = 2005;
+  bool isActive = true;
+  print("nama :$nama");
+  print("umur :$umur");
+  print("tinggi :$tinggi");
+  print("kelahiran :$kelahiran");
+  print("mahasiswa ini statusnya : $isActive");
+}
